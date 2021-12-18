a5cc1894bc2db2367eb2d9ff3f4f229948f0f3aa
kirillvoroncov <kir-v@bk.ru>
2021-12-18 21:50:31 +0300

    git

--- main.py
+++ main.py
@@ -4,61 +4,65 @@ import json
 def main():
     """Основная функция: выполняет прогон всех модулей"""
 
-    for i in range(1, 100, 1):
+    global conf
+    import conf
+    c = conf.MODEL
 
-        global conf
-        import conf
-        c = conf.MODEL
+    global random
+    import random
 
-        global random
-        import random
-
-        # выводим название книги из текстового файла
-
+    # выводим название книги из текстового файла
+    def title():
         contents = []
 
         with open(" books.txt") as rnd:
             for line in rnd:
                 line = line.strip()
                 contents.append(line)
-                b = contents[random.randint(0, len(contents) - 1)]
+        return contents[random.randint(0, len(contents) - 1)]
 
         # выводим случайное число (год)
+    def year():
         from random import randint
-        d = randint(1900, 2000)
+        return randint(1900, 2000)
 
         # выводим случайное число (страницы)
+    def pages():
         from random import randint
-        e = randint(100, 200)
+        return randint(100, 200)
 
         # выводим случайный книжный номер
+    def isbn():
         from faker import Faker
         fake = Faker()
-        f = fake.isbn13()
+        return fake.isbn13()
 
         # выводим случайный рейтинг
-        g = round(random.uniform(1, 5),2)
+    def rating():
+        return round(random.uniform(1, 5),2)
 
         # выводим случайную цену
-        m = round(random.uniform(500, 3000),2)
+    def price():
+        return round(random.uniform(500, 3000),2)
 
         # выводим случайных авторов
-
+    def authors():
+        from faker import Faker
         fake = Faker(locale="ru_RU")
-        s = fake.name()
+        return fake.name()
 
-        a = dict(model=c,
-                 title=b,
-                 year=d,
-                 pages=e,
-                 isbn13=f,
-                 rating=g,
-                 price=m,
-                 authors=s)
+    a = dict(model=c,
+             title=title(),
+             year=year(),
+             pages=pages(),
+             isbn13=isbn(),
+             rating=rating(),
+             price=price(),
+             authors=authors())
 
-        json_data = json.dumps(a, indent=4, ensure_ascii=False)
-        with open("knigi.json", "a") as file:
-            file.writelines(json_data)
+    json_data = json.dumps(a, indent=4, ensure_ascii=False)
+    with open("knigi.json", "w") as file:
+        file.writelines(json_data)
 
 
 if __name__ == '__main__':
